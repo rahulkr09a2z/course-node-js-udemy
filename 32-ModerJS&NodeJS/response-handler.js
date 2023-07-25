94be1f70e68e33ced83c4af4032c5dad30a84109
rahulkr09a2z <rahulkr123a2z@gmail.com>
2023-07-25 18:12:20 +0530

    [Module-32]|1)fs promises demo

--- 32-ModerJS&NodeJS/response-handler.js
+++ 32-ModerJS&NodeJS/response-handler.js
@@ -1,9 +1,23 @@
-import fs from "fs";
+// import fs from "fs";
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  //     fs.readFile("my-page.html", "utf8", (err, data) => {
+  //     res.send(data);
+  // });
+  fs.readFile("my-page.html", "utf8")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // module.exports = resHandler;
